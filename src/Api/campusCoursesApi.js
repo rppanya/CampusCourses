a2a1fd3429b08a297598629156147460cc68450b
Rapp Anna <anjarapp18@gmail.com>
2023-03-29 23:44:40 +0700

    create react app

---
+++ src/Api/campusCoursesApi.js
@@ -0,0 +1,8 @@
+import * as axios from 'axios'
+
+const baseURL = ''
+
+const instance = axios.create({
+    baseURL : baseURL
+});
+
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { Button, Breadcrumb, Layout, Menu, theme } from 'antd';
+import 'antd/dist/reset.css';
+import Navbar from './components/navbar/navbar';
+import { Route, Routes } from 'react-router-dom';
+
+const { Header, Content, Footer } = Layout;
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Layout className="layout">
+        
+      </Layout>
+    </div>
+  );
+}
+
+export default App;

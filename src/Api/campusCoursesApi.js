a807632b7251648166cef1216daa4504ec4a9eda
Rapp Anna <anjarapp18@gmail.com>
2023-04-08 13:40:24 +0700

    added account requests

--- src/Api/campusCoursesApi.js
+++ src/Api/campusCoursesApi.js
@@ -3,12 +3,10 @@ import axios from "axios";
 const baseURL = "https://camp-courses.api.kreosoft.space/";
 
 const instance = axios.create({
-  baseURL: baseURL,
+  baseURL: baseURL
 });
 
 function login(data) {
-  console.log("api");
-
   return instance
     .post("login", data, {
       headers: {
@@ -28,6 +26,7 @@ function login(data) {
 }
 
 function registration(data) {
+  console.log("registrationApi")
     return instance
         .post('registration', data, {
             headers: {
@@ -46,7 +45,70 @@ function registration(data) {
         })
 }
 
+function logout(token) {
+    return instance
+        .post('logout', {}, {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                'Authorization': 'Bearer ' + token,
+              },
+        })
+        .then((response) => {
+            if (response.status === 200) {
+                return response
+            }
+        })
+        .catch((error) => {
+            console.log(error.response)
+        })
+
+}
+
+function getProfileInfo(token) {
+  return instance
+      .get('profile', {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          'Authorization': 'Bearer ' + token,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.data
+        }
+      })
+      .catch((error) => {
+        console.log(error.response)
+      })
+}
+
+function changeProfileInfo(data, token) {
+  console.log(token)
+  data = JSON.stringify(data)
+  return instance
+      .put('profile', data, {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          'Authorization': 'Bearer ' + token,
+        }
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          return response
+        }
+      })
+      .catch((error) => {
+        console.log(error.response)
+      })
+}
+
 export const campusCoursesApi = {
   login: login,
-  registration: registration
+  registration: registration,
+  logout: logout,
+  getProfileInfo: getProfileInfo,
+  changeProfileInfo: changeProfileInfo
 }
